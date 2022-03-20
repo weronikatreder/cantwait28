f35c64a0b2026667efdc9db46ad908cdd9bc0035
Weronika Treder <wtreder53@gmail.com>
2022-03-20 09:51:30 +0100

    Add details page

---
+++ lib/features/details/cubit/details_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_model.dart';
+import 'package:cantwait28/repositories/items_repository.dart';
+
+part 'details_state.dart';
+
+class DetailsCubit extends Cubit<DetailsState> {
+  DetailsCubit(this._itemRepository) : super(DetailsState(itemModel: null));
+
+  final ItemRepository _itemRepository;
+
+  Future<void> getItemWithID(String id) async {
+    final itemModel = await _itemRepository.get(id: id);
+    emit(DetailsState(itemModel: itemModel));
+  }
+}
